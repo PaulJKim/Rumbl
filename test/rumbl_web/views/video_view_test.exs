a4606279bca3beaba66df1e800ba31b15fd2cc8a
Paul Kim <pauljk704@gmail.com>
2023-02-22 21:12:44 -0500

    fix view tests

--- test/rumbl_web/views/video_view_test.exs
+++ test/rumbl_web/views/video_view_test.exs
@@ -2,31 +2,34 @@ defmodule RumblWeb.VideoViewTest do
   use RumblWeb.ConnCase, async: true
   import Phoenix.View
 
-  test "renders index.html" do
+  test "renders index.html", %{conn: conn} do
     videos = [
       %Rumbl.Multimedia.Video{id: "1", title: "dogs"},
       %Rumbl.Multimedia.Video{id: "2", title: "cats"}
     ]
 
-    content = render_to_string(
-      RumblWeb.VideoView,
-      "index.html",
-      conn: conn,
-      videos: videos
-    )
+    content =
+      render_to_string(
+        RumblWeb.VideoView,
+        "index.html",
+        conn: conn,
+        videos: videos
+      )
 
     assert String.contains?(content, "Listing Videos")
 
     for video <- videos do
       assert String.contains?(content, video.title)
     end
+  end
 
-    test "renders new.html" do
-      owner = %Rumbl.Accounts.User{}
-      changeset = Rumbl.Multimedia.change_video(%Rumbl.Multimedia.Video{})
-      categories = [%Rumbl.Multimedia.Category{id: 123, name: "cats"}]
+  test "renders new.html", %{conn: conn} do
+    owner = %Rumbl.Accounts.User{}
+    changeset = Rumbl.Multimedia.change_video(%Rumbl.Multimedia.Video{})
+    categories = [%Rumbl.Multimedia.Category{id: 123, name: "cats"}]
 
-      content = render_to_string(
+    content =
+      render_to_string(
         RumblWeb.VideoView,
         "new.html",
         conn: conn,
@@ -34,6 +37,6 @@ defmodule RumblWeb.VideoViewTest do
         categories: categories
       )
 
-      assert String.contains?(content, "New Video")
-    end
+    assert String.contains?(content, "New Video")
+  end
 end
